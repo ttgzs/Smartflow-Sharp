b549e830331357742d73e2fd624cb40cb10e4f2c
chengderen <chengderen@users.noreply.github.com>
2018-04-10 21:48:39 +0800

    chengderen

---
+++ ConsoleApplication.Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using Smartflow;
+using Smartflow.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //首次启动工作流程
+            //WorkflowEngine engine = WorkflowEngine.CreateWorkflowEngine("1");
+            WorkflowEngine engine = WorkflowEngineExt.CreateWorkflowEngine();
+            engine.OnProcess = (exectionContext) =>
+             {
+
+                 Console.WriteLine(exectionContext.Data.Name);
+
+                 Console.WriteLine(string.Format("instanceID:{0} From:{1} To:{2}", exectionContext.Instance.InstanceID, exectionContext.From.NAME, exectionContext.To.NAME));
+             };
+
+            engine.OnCompleted = (exectionContext) =>
+            {
+                Console.WriteLine("流程结束");
+            };
+
+            WorkflowInstance instance = engine.GetWorkflowInstance("8accf23a-48d7-4e9f-8e3a-ed05fb102fe4");
+            ASTNode currentNode = instance.Current;
+
+            if (instance.Current.NodeType == Smartflow.Enums.WorkflowNodeCategeory.End)
+            {
+                Console.WriteLine("流程结束");
+            }
+            else
+            {
+                Transition tran = currentNode.Transitions.First();
+                engine.Jump(instance, tran.NID, tran.TO, data:new { Name="程德忍" });
+
+               // engine.Jump(instance, tran.NID, instance.Current.PreviousTransition.FROM);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication.Test/WorkflowEngineExt.cs
@@ -0,0 +1,24 @@
+﻿using Smartflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication.Test
+{
+    class WorkflowEngineExt : WorkflowEngine
+    {
+
+        protected override bool CheckAuthorization(WorkflowInstance instance, long actorID)
+        {
+
+            return true;
+
+        }
+
+        public static WorkflowEngine CreateWorkflowEngine()
+        {
+            return new WorkflowEngineExt();
+        }
+    }
+}
