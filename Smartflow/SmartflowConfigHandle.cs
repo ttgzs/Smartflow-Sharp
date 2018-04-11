b10a2825e29a826e650421c1050ba0c24ec0e3fc
chengderen <chengderen@users.noreply.github.com>
2018-04-11 21:35:27 +0800

    将数据库、连接改成可以配置化；增加设置人员方法。

---
+++ Smartflow/SmartflowConfigHandle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Smartflow
+{
+    public class SmartflowConfigHandle : ConfigurationSection
+    {
+        [ConfigurationProperty("connectionString", IsRequired = true)]
+        public string ConnectionString
+        {
+            get { return this["connectionString"].ToString(); }
+            private set { }
+        }
+
+        [ConfigurationProperty("category", IsRequired = true, DefaultValue = "SQLServer")]
+        public string DatabaseCategory
+        {
+            get { return this["category"].ToString(); }
+            private set { }
+        }
+    }
+}
