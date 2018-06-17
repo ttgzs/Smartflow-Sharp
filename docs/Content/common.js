d06fa04abcf6f25d1895da9bc2872f3a8e53c8a7
chengderen <chengderen@users.noreply.github.com>
2018-06-18 06:42:41 +0800

    chengderen

---
+++ docs/Content/common.js
@@ -0,0 +1,6 @@
+﻿function openWin(url, width, height) {
+    var iTop = (window.screen.availHeight - 30 - height) / 2;
+    var iLeft = (window.screen.availWidth - 10 - width) / 2;
+    window.open(url, 'newwindow', "height=" + height + ", width=" + width + ", top=" + iTop + " left=" + iLeft + ", toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no");
+}
+
